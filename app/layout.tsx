b4c87edc0624603e87222d907712239548af3aab
dev-shajid <sajidislam729@gmail.com>
2024-12-12 22:03:02 +0600

    feat: enhance metadata and add rewrites for improved SEO and resource handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,11 @@ import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import Head from "next/head";
+import type { Metadata } from "next";
 
 const font = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Attensys | AI",
   description: "Attensys is a platform that provides AI-powered solutions to businesses.",
   icons: '/icons/favicon.ico',
@@ -42,15 +43,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content="Attensys | AI" />
-        <meta property="og:description" content="Attensys is a platform that provides AI-powered solutions to businesses." />
-        <meta property="og:image" content="/icons/landing.png" />
-        <meta property="twitter:title" content="Attensys | AI" />
-        <meta property="twitter:description" content="Attensys is a platform that provides AI-powered solutions to businesses." />
-        <meta property="twitter:image" content="/icons/landing.png" />
-        <link rel="icon" href={metadata.icons} />
+        <title key="title">Attensys | AI</title>
+        <meta key="keywords" name="keywords" content={`Attensys, AI Chatbot`} />
+        <meta key="description" name="description" content={"Attensys is a platform that provides AI-powered solutions to businesses."} />
+        <meta key="og-title" property="og:title" content="Attensys | AI" />
+        <meta key="og-description" property="og:description" content={"Attensys is a platform that provides AI-powered solutions to businesses."} />
+        <meta key="og-url" property="og:url" content={`https://chat-bot-attensys.vercel.app`} />
+        <meta key="twitter-title" name="twitter:title" content={"Attensys | AI"} />
+        <meta key="twitter-description" name="twitter:description" content={"Attensys is a platform that provides AI-powered solutions to businesses."} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@attensys" />
+        <meta name="twitter:image" content={`https://chat-bot-attensys.vercel.app/icons/landing.png/`} />
+        <link rel="canonical" href="https://chat-bot-attensys.vercel.app/" />
       </Head>
       <body
         className={cn(
--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,21 @@ const nextConfig = {
     domains: ["utfs.io", "subdomain"],
   },
   reactStrictMode: false,
+  rewrites: async () => [
+    {
+      source: "/:path*",
+      destination: "/:path*",
+    },
+    // Static files of the App
+    {
+      source: "/icons/favicon.ico",
+      destination: `${destinationHost}/icons/favicon.ico`,
+    },
+    {
+      source: "/icons/:path*",
+      destination: `${destinationHost}/icons/:path*`,
+    },
+  ]
 };
 
 export default nextConfig;
