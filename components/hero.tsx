b3b7fe95993ec33ad773676d6a8df61e8a0647c2
dev-shajid <sajidislam729@gmail.com>
2024-12-15 21:46:39 +0600

    fix: optimize animation delays and improve video container settings in Hero component

--- components/hero.tsx
+++ components/hero.tsx
@@ -16,24 +16,24 @@ const Hero = () => {
 
       <div className="flex flex-col items-center justify-center py-4 h-full">
         <div className="flex flex-col items-center mt-8 max-w-3xl w-11/12 md:w-full">
-          <Container delay={0.4}>
+          <Container delay={0}>
             <AnimatedSplitText
               className="text-4xl md:text-5xl lg:text-5xl md:!leading-snug font-semibold text-center bg-clip-text text-transparent bg-gradient-to-r from-white via-white to-neutral-500"
               text={"Transforming Business Operations with Smarter"}
-              delay={0.4}
+              delay={0}
             />
           </Container>
-          <Container delay={0.7}>
+          <Container delay={0.3}>
             <AnimatedSplitText
               className="text-4xl md:text-5xl lg:text-5xl md:!leading-snug font-semibold text-center bg-gradient-to-r from-[#5c71f6] to-[#7646ef] text-transparent bg-clip-text"
               text={"AI Agents"}
-              delay={1}
+              delay={0.3}
             />
           </Container>
-          <Container className="mt-12" delay={1} reverse={false}>
-            <AnimatedSplitText text={description} delay={1} />
+          <Container className="mt-12" delay={0.6} reverse={false}>
+            <AnimatedSplitText text={description} delay={0.0} />
           </Container>
-          <Container delay={1.3} className="flex justify-center mt-12">
+          <Container delay={0.9} className="flex justify-center mt-12">
             <Link
               href="#"
               className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200"
@@ -50,7 +50,7 @@ const Hero = () => {
             </Link>
           </Container>
         </div>
-        <Container delay={1.6} className="w-full bg-gradient-to-b from-transparent to-black opacity-50 -z-10">
+        <Container delay={1.2} className="w-full bg-gradient-to-b from-transparent to-black opacity-50 -z-10">
           <div className="relative flex items-center py-10 md:py-20 w-full ">
             <video
               className="w-full h-full md:max-h-[85dvh] aspect-[838/471] rounded-xl"
